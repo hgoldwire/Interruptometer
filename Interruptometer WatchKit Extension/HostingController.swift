697a4272d5e01ebb95561c21a9749a3dea703e54
Henry Goldwiwe <henry.goldwire@roivant.com>
2019-10-11 17:07:40 -0700

    Initial Commit

---
+++ Interruptometer WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Interruptometer WatchKit Extension
+//
+//  Created by Henry Goldwire on 10/11/19.
+//  Copyright © 2019 Henry Goldwire. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ Interruptometer WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  Interruptometer WatchKit Extension
+//
+//  Created by Henry Goldwire on 10/11/19.
+//  Copyright © 2019 Henry Goldwire. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
